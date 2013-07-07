871bd98e7937cc922ae8d80aaed69dfd934ffa47
zenteo <zenteo@debox>
2013-07-07 21:07:49 +0200

    Did some net stuff

---
+++ src/org/xeroworld/slimestory/net/Packet.java
@@ -0,0 +1,7 @@
+package org.xeroworld.slimestory.net;
+
+public class Packet {
+	private int id;
+	private int responceTo;
+	
+}
--- src/org/xeroworld/slimestory/server/Server.java
+++ src/org/xeroworld/slimestory/server/Server.java
@@ -1,5 +1,67 @@
 package org.xeroworld.slimestory.server;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server implements Runnable {
+	private ServerSocket socket;
+	private Thread acceptThread;
+	private boolean running;
+	
+	public Server(int port) {
+		try {
+			socket = new ServerSocket(port);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void mainloop() {
+		while (running) {
+			try {
+				Thread.sleep(10);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void handleClient(Socket client) {
+		
+	}
+	
+	@Override
+	public void run() {
+		while (running) {
+			try {
+				Socket client = socket.accept();
+				handleClient(client);
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void start() {
+		running = true;
+		if (acceptThread == null) {
+			acceptThread = new Thread(this);
+			acceptThread.start();
+		}
+		mainloop();
+	}
+	
+	public void stop() {
+		running = false;
+		acceptThread = null;
+	}
+	
+	public static void main(String[] args) {
+		Server server = new Server(5340);
+		server.start();
+	}
 }
